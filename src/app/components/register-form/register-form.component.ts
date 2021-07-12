22997a2663e9e9ef9698b775363feb067c176fa4
Nikola Cichewicz <cichewicznikola@wp.pl>
2021-07-12 11:54:13 +0200

    abc

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -8,8 +8,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterFormComponent implements OnInit {
 
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";  //Pattern to check if email is correct
-  
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';  //Pattern to check if email is correct
+
   registerForm;
 
   disableForm = true;
@@ -23,33 +23,33 @@ export class RegisterFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.registerForm = this.formBuilder.group({
-      username: [{value:'', disabled:this.disableForm}],
-      email: [{value:'', disabled:this.disableForm}],
-      password: [{value:'', disabled:this.disableForm}],
-      confirmpassword: [{value:'', disabled:this.disableForm}],
-      dropdown:[{value:'', disabled:this.disableForm}],
-      checkbox: [{value:'', disabled:this.disableForm}],
+      username: [{value: '', disabled: this.disableForm}],
+      email: [{value: '', disabled: this.disableForm}],
+      password: [{value: '', disabled: this.disableForm}],
+      confirmpassword: [{value: '', disabled: this.disableForm}],
+      dropdown: [{value: '', disabled: this.disableForm}],
+      checkbox: [{value: '', disabled: this.disableForm}],
     });
   }
 
-  
+
   ngOnInit(): void {
     this.itemsToShow = this.getFilteredItems([this.itemIdToHide]);
 
     this.checkbox.valueChanges.subscribe((checked) => {
-        if (checked) {
-            this.itemsToShow = [...this.items];
-        } else {
-            this.itemsToShow = this.getFilteredItems([this.itemIdToHide]);
-        }
+      if (checked) {
+        this.itemsToShow = [...this.items];
+      } else {
+        this.itemsToShow = this.getFilteredItems([this.itemIdToHide]);
+      }
     });
-}
+  }
 
-private getFilteredItems(idsToHide: number[]): { id: number; name: string }[] {
+  private getFilteredItems(idsToHide: number[]): { id: number; name: string }[] {
     return this.items.filter((item) => !idsToHide.includes(item.id));
-}
+  }
 
-  enableEdit(){
+  enableEdit() {
     this.disableForm = !this.disableForm;
     // if(!this.disableForm) this.registerForm.disable();
     this.registerForm.enable();
@@ -60,38 +60,42 @@ private getFilteredItems(idsToHide: number[]): { id: number; name: string }[] {
     {
       id: 1,
       name: 'Option 1'
-    }, 
+    },
     {
       id: 2,
       name: 'Option 2'
-    }, 
+    },
     {
       id: 3,
       name: 'Option 3'
-    }, 
+    },
     { //extra option
       id: 4,
       name: 'Option 4'
     },
-  ]
+  ];
 
- 
 
   get username() {
     return this.registerForm.get('username');
   }
+
   get email() {
     return this.registerForm.get('email');
   }
+
   get password() {
     return this.registerForm.get('password');
   }
+
   get confirmpassword() {
     return this.registerForm.get('confirmpassword');
   }
+
   get dropdown() {
     return this.registerForm.get('dropdown');
   }
+
   get checkbox() {
     return this.registerForm.get('checkbox');
   }
