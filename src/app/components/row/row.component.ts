90556f034065e90d6423477ae3891c3642c4a6de
Nikola Cichewicz <n.n.cichewicz@gmail.com>
2021-07-17 19:31:17 +0200

    + input3 required fixed

--- src/app/components/row/row.component.ts
+++ src/app/components/row/row.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit,AfterViewInit} from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, FormArray,ControlContainer, ValidatorFn, AbstractControl} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-row',
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, FormControl, Validators, FormArray,ControlConta
 export class RowComponent implements OnInit {
 
   @Input() rowForm: FormGroup;
+  subscription: Subscription;
   showInput = false;
 
 
@@ -19,31 +21,45 @@ export class RowComponent implements OnInit {
     setTimeout(()=>{
       this.rowForm.addControl("rows",this.formBuilder.array([this.createRow(true)]))
 
-    })
+    });
+    const checkbox2 = <FormControl>this.rowForm.get('checkbox2');
+    const input3 = <FormControl>this.rowForm.get('input3');
+
+    this.subscription = checkbox2.valueChanges.subscribe(value => {
+      if (value) {
+        input3.setValidators([Validators.required, ])
+      }
+      else {
+        input3.setValidators(null);
+      }
+
+      input3.updateValueAndValidity();
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 
   createRow(disabled:boolean=false): FormGroup {
     return this.formBuilder.group({
       input1: [{value:'',disabled:disabled}, Validators.required],
       input2: [{value:'',disabled:disabled}, Validators.required],
-      input3: [{value:'',disabled:disabled}, Validators.required],
-      checkbox2: [{value:'',disabled:disabled}, Validators.required],
+      input3: [{value:'',disabled:disabled}],
+      checkbox2: [{value:'',disabled:disabled}],
     });
   }
 
-
   toggleInput(index: number) {
     this.showInput = !this.showInput;
-    this.rowForm.controls.input3.setValidators(this.showInput ? null : [Validators.required]);
-    this.rowForm.controls.input3.updateValueAndValidity();
   }
 
   get formArray() {
     return this.rowForm.get('rows') as FormArray;
   }
 
-  // Adding row function
   addNewRow() {
     this.formArray.push(this.createRow());
   }
@@ -55,5 +71,4 @@ export class RowComponent implements OnInit {
   getControls() {
     return (this.rowForm.get('rows') as FormArray).controls;
   }
-
 }
